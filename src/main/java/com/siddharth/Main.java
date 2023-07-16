48c4537c5d69d8f2e0126136942a1142bfee9ec1
siddharth-nair-2 <nairsid2@gmail.com>
2023-07-16 18:31:40 -0400

    trying out records for responses and GetMapping

--- src/main/java/com/siddharth/Main.java
+++ src/main/java/com/siddharth/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -13,7 +15,45 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public String greet() {
-        return "Hello";
+    public GreetResponse greet() {
+        return new GreetResponse("Hello!",
+                List.of("Java", "Golang", "JavaScript"),
+                new Person("Siddharth", 21, 12_412.43));
     }
+
+    record Person(String name, int age, double savings) {
+
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person) {
+    }
+
+//    static class GreetResponse {
+//        private final String greet;
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return super.toString();
+//        }
+//
+//        @Override
+//        public boolean equals(Object obj) {
+//            return super.equals(obj);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return super.hashCode();
+//        }
+//    }
 }
