2c91251a71b0d59de9dee99ac08d7a55ed2f580d
siddharth-nair-2 <nairsid2@gmail.com>
2023-07-16 22:37:01 -0400

    Trying out Bean, and their scopes

--- src/main/java/com/siddharth/Main.java
+++ src/main/java/com/siddharth/Main.java
@@ -2,12 +2,35 @@ package com.siddharth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+
+        printBeans(applicationContext);
+    }
+
+    @Bean
+    public Foo getFoo() {
+        return new Foo("bar");
+    }
+
+    record Foo(String name) {
+    }
+
+    private static void printBeans(ConfigurableApplicationContext ctx) {
+
+        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+
+        for (String beanDef :
+                beanDefinitionNames) {
+            System.out.println(beanDef);
+        }
     }
 
 }
