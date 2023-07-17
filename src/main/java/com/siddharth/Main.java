6e5204a9d052e413446cbf50eff8b00c42ca270c
siddharth-nair-2 <nairsid2@gmail.com>
2023-07-17 10:04:19 -0400

    Set up docker container and updated application.yml for postgresql use

--- src/main/java/com/siddharth/Main.java
+++ src/main/java/com/siddharth/Main.java
@@ -10,27 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
-
-        printBeans(applicationContext);
-    }
-
-    @Bean
-    public Foo getFoo() {
-        return new Foo("bar");
-    }
-
-    record Foo(String name) {
-    }
-
-    private static void printBeans(ConfigurableApplicationContext ctx) {
-
-        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
-
-        for (String beanDef :
-                beanDefinitionNames) {
-            System.out.println(beanDef);
-        }
+        SpringApplication.run(Main.class, args);
     }
 
 }
--- src/main/java/com/siddharth/customer/CustomerService.java
+++ src/main/java/com/siddharth/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.siddharth.customer;
 
+import com.siddharth.exception.ResourceNotFound;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,7 +20,9 @@ public class CustomerService {
 
     public Customer getCustomer(Integer id) {
         return customerDao.selectCustomerById(id)
-                .orElseThrow(() -> new IllegalArgumentException("customer with id [%s] not found".formatted(id)));
+                .orElseThrow(() -> new ResourceNotFound(
+                        "customer with id [%s] not found".formatted(id)
+                ));
 
     }
 }
