0416e269a3135e01dcafea66c4b1fcbbcf4ab33a
siddharth-nair-2 <nairsid2@gmail.com>
2023-07-18 21:02:44 -0400

    Updated pom.xml to include Testcontainers

---
+++ src/test/java/com/siddharth/TestcontainersTest.java
@@ -0,0 +1,15 @@
+package com.siddharth;
+
+import org.junit.jupiter.api.Test;
+
+public class TestcontainersTest {
+
+    @Test
+    void canStartPostgresDB() {
+        // Given
+
+        // When
+
+        //Then
+    }
+}
