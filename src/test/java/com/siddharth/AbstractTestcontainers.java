1db640537388bd0ae17a5052b27f1b61f529ca7a
Siddharth Nair <nairsid2@gmail.com>
2023-07-25 10:39:34 -0400

    Abstract test containers for reuse.

---
+++ src/test/java/com/siddharth/AbstractTestcontainers.java
@@ -0,0 +1,60 @@
+package com.siddharth;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+
+@Testcontainers
+public abstract class AbstractTestcontainers {
+
+    @BeforeAll
+    static void beforeAll() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+    }
+
+    @Container
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("siddharth-dao-unit-test")
+                    .withUsername("siddharth")
+                    .withPassword("password");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.datasource.url",
+                postgreSQLContainer::getJdbcUrl
+        );
+        registry.add(
+                "spring.datasource.username",
+                postgreSQLContainer::getUsername
+        );
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
+
+    protected static DataSource getDataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword());
+
+        return builder.build();
+    }
+
+}
