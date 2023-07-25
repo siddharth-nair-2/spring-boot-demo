0173bc9a51f253189042d8651568e6585efd7e4e
Siddharth Nair <nairsid2@gmail.com>
2023-07-25 18:32:00 -0400

    Mock test CustomerJPADataAccessService with 100% coverage

---
+++ src/test/java/com/siddharth/customer/CustomerJPADataAccessServiceTest.java
@@ -0,0 +1,119 @@
+package com.siddharth.customer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+class CustomerJPADataAccessServiceTest {
+
+    private CustomerJPADataAccessService underTest;
+    private AutoCloseable autoCloseable;
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable =  MockitoAnnotations.openMocks(this);
+        underTest = new CustomerJPADataAccessService(customerRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception{
+        autoCloseable.close();
+    }
+
+    @Test
+    void selectAllCustomers() {
+        // When
+        underTest.selectAllCustomers();
+
+        // Then
+        verify(customerRepository).findAll();
+    }
+
+    @Test
+    void selectCustomerById() {
+        // Given
+        int id = 1;
+
+        // When
+        underTest.selectCustomerById(id);
+
+        // Then
+        verify(customerRepository).findById(id);
+    }
+
+    @Test
+    void insertCustomer() {
+        // Given
+        Customer customer = new Customer(
+                3,
+                "Sid",
+                "sid@gmail.com",
+                20
+        );
+
+        // When
+        underTest.insertCustomer(customer);
+
+        // Then
+        verify(customerRepository).save(customer);
+    }
+
+    @Test
+    void existsCustomerWithEmail() {
+        // Given
+        String email = "abc@def.com";
+
+        // When
+        underTest.existsCustomerWithEmail(email);
+
+        // Then
+        verify(customerRepository).existsCustomerByEmail(email);
+    }
+
+    @Test
+    void deleteCustomer() {
+        // Given
+        int id = 1;
+
+        // When
+        underTest.deleteCustomer(id);
+
+        // Then
+        verify(customerRepository).deleteById(id);
+    }
+
+    @Test
+    void existsCustomerWithId() {
+        // Given
+        int id = 1;
+
+        // When
+        underTest.existsCustomerWithId(id);
+
+        // Then
+        verify(customerRepository).existsCustomerById(id);
+    }
+
+    @Test
+    void updateCustomer() {
+        // Given
+        Customer customer = new Customer(
+                3,
+                "Sid",
+                "sid@gmail.com",
+                20
+        );
+
+        // When
+        underTest.updateCustomer(customer);
+
+        // Then
+        verify(customerRepository).save(customer);
+    }
+}
