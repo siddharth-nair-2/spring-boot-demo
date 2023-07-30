96c63d784b4b98e404b62b64f8c5c468ca91e1f2
Siddharth Nair <nairsid2@gmail.com>
2023-07-29 21:18:44 -0400

    Remove unused import.

--- backend/src/main/java/com/siddharth/Main.java
+++ backend/src/main/java/com/siddharth/Main.java
@@ -1,23 +1,19 @@
 package com.siddharth;
 
 import com.github.javafaker.Faker;
-import com.github.javafaker.Name;
 import com.siddharth.customer.Customer;
 import com.siddharth.customer.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
     }
 
--- backend/src/main/java/com/siddharth/customer/CustomerUpdateRequest.java
+++ backend/src/main/java/com/siddharth/customer/CustomerUpdateRequest.java
@@ -1,7 +1,5 @@
 package com.siddharth.customer;
 
-import java.util.Optional;
-
 public record CustomerUpdateRequest(
         String name,
         String email,
