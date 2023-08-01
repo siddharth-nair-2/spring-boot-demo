4c9d18c6e8d7d4261a454752c12f51decd598750
Siddharth Nair <nairsid2@gmail.com>
2023-07-31 23:59:14 -0400

    Add frontend folder for react. Set up using vite, add tailwind and daisyUI.

---
+++ frontend/react/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <button className="btn btn-secondary w-64 rounded-full">
+        please Dont be Caps
+      </button>
+    </div>
+  );
+}
+
+export default App;
