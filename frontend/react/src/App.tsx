618e6efbb5844f5cf7d699bd92f80bcd13b75175
Siddharth Nair <nairsid2@gmail.com>
2023-08-01 18:42:23 -0400

    add react libraries, and setup cors on backend

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,10 +1,53 @@
+import { Spinner, Text } from "@chakra-ui/react";
+import SidebarWithHeader from "./components/shared/Sidebar";
+import { useEffect, useState } from "react";
+import { getCustomers } from "./services/client";
+import { Customer } from "./interface/customer";
+import ProfileCard from "./components/shared/ProfileCard";
+
 function App() {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getCustomers()
+      .then((res) => {
+        console.log(res.data);
+        setCustomers(res.data);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <SidebarWithHeader>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </SidebarWithHeader>
+    );
+  }
+
+  if (customers.length <= 0) {
+    return (
+      <SidebarWithHeader>
+        <Text>No Customers found!</Text>
+      </SidebarWithHeader>
+    );
+  }
+
   return (
-    <div>
-      <button className="btn btn-secondary w-64 rounded-full">
-        please Dont be Caps
-      </button>
-    </div>
+    <SidebarWithHeader>
+      {customers.map((customer) => {
+        return <ProfileCard customer={customer} />;
+      })}
+    </SidebarWithHeader>
   );
 }
 
